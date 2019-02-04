c740751153ad5b3c38cf7bc09ebf77df1b33389b
barnzilla <barnzilla@barnzilla.ca>
2019-02-04 14:00:00 -0500

    Simplified code by removing need for s object.

--- Recursive function.R
+++ Recursive function.R
@@ -4,13 +4,11 @@ recursive_sum_function <- function( v ) {
 
 	# Input: v, numeric vector
 	# Return sum of all elements in v
-
-	if( exists( "s" ) ) s <- s + v[ length( v ) ] else s <- 0
-
+	
 	if( length( v ) == 1) {
-		return( s )
+		return( v )
 	} else {
-		return( s + recursive_sum_function( v[ 1:length( v ) - 1 ] ) )
+		return( v[ length( v ) ] + recursive_sum_function( v[ 1:length( v ) - 1 ] ) )
 	}
 
 }
